27f48a8718d5d782d140c1211edae28ecc509aec
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2021-12-17 18:50:59 +0700

    Jawaban soal E

---
+++ soal/e.py
@@ -0,0 +1,25 @@
+#Solusi optimal
+if __name__ == '__main__':
+    n = int(input())
+    
+    if(n % 2 == 1):
+        print('Weird')
+    elif(n >= 2 and n <= 5):
+        print('Not Weird')
+    elif(n >= 6 and n <= 20):
+        print('Weird')
+    else:
+        print('Not Weird')
+ 
+#Solusi lain (lebih banyak yang diketik)
+if __name__ == '__main__':
+    n = int(input())
+    
+    if(n % 2 == 1):
+        print('Weird')
+    if(n % 2 == 0 and n >= 2 and n <= 5):
+        print('Not Weird')
+    if(n % 2 == 0 and n >= 6 and n <= 20):
+        print('Weird')
+    if(n % 2 == 0 and n > 20):
+        print('Not Weird')
