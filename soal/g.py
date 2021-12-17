8cbd8271bed9fa26f5cf9725289448ee548ca93d
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2021-12-17 18:53:22 +0700

    Jawaban soal G

---
+++ soal/g.py
@@ -0,0 +1,11 @@
+#Solusi dengan i dimulai dari 0 hingga n-1
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i+1, end="")
+
+#Solusi dengan i dimulai dari 1 hingga n
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end="")
